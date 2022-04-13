fa78af94328bc79d2a7ef26ccce0f9021a9b051c
SharmaGSaroj <91160959+SharmaGSaroj@users.noreply.github.com>
2022-04-13 13:00:13 -0400

    ErrorFixed

--- js/main.js
+++ js/main.js
@@ -1,74 +1,86 @@
 const musicboard = document.querySelector("#buttonHolder img"),
-    instrument = document.querySelectorAll('.instruments *'),
-    dropZones = document.querySelectorAll(".drop-zone"),
-    Audio = document.querySelectorAll("audio *"),
-    playbtn= document.getElementById('playbtn'),
-    pausebtn= document.getElementById('stopbtn'),
-    rewindbtn= document.getElementById('rewindbtn'),
-    dragL = document.querySelector(".instruments"),
-    AudioEl = document.querySelectorAll("audio");
+  instrument = document.querySelectorAll('.instruments *'),
+  dropZones = document.querySelectorAll(".drop-zone"),
+  Audio = document.querySelectorAll("audio *"),
+  playbtn = document.getElementById('playbtn'),
+  pausebtn = document.getElementById('stopbtn'),
+  rewindbtn = document.getElementById('rewindbtn'),
+  dragL = document.querySelector(".instruments"),
+  AudioEl = document.querySelectorAll("audio");
 
 
-    function allowdrag(event) {
-        event.dataTransfer.setData('currentItem', event.target.id);
-        event.dataTransfer.setData('thetrack', event.target.dataset.bgref);
+function allowdrag(event) {
+  event.dataTransfer.setData('currentItem', event.target.id);
 
+
+}
+function allowDragOver(event) {
+  event.preventDefault();
+  console.log('dragged over me');
+}
+function allowDrop(event) {
+  event.preventDefault();
+  console.log('dropped on me')
+
+  if (this.childElementCount == 1) { return; }
+  console.log('space filled');
+
+  let droppedEl = event.dataTransfer.getData('currentItem');
+  console.log(droppedEl);
+
+
+
+  let music = document.querySelector(`#${droppedEl}`);
+  this.appendChild(music);
+  /*  let track = event.dataTransfer.getData('thetrack');
+
+    document.querySelector(`audio[data-bgref="${track}"]`).play();*/
+  let track = document.createElement('audio');
+
+  track.loop = true;
+
+  track.src = `audio/${music.dataset.bgref}.wav`;
+  track.load();
+
+  document.body.appendChild(track);
+  track.play();
+}
+
+
+function playsound() {
+  let sounds = document.querySelectorAll('audio');
+  sounds.forEach(mixer => mixer.play());
+
+}
+
+function pausesound() {
+  let sounds = document.querySelectorAll('audio');
+  sounds.forEach(mixer => mixer.pause());
+
+}
+
+function rewindsound() {
+  var sounds = document.getElementsByTagName("audio");
+  for (i = 0; i < sounds.length; i++) sounds[i].currentTime = 0;
+
+  console.log('rewinded')
+}
+
+
+function resetbtn() {
+  dropZones.forEach(zone => {
+    if (zone.childElementCount > 0) {
+      dragL.appendChild(zone.firstElementChild);
     }
-    function allowDragOver(event){
-        event.preventDefault();
-        console.log('dragged over me');
-    }
-    function allowDrop(event){
-        event.preventDefault();
-        console.log('dropped on me')
+  })
+  let sounds = document.querySelectorAll('audio');
+  sounds.forEach(sound => document.body.removeChild(sound));
+}
 
 
 
-        let droppedEl = event.dataTransfer.getData('currentItem');
-        console.log(droppedEl);
+instrument.forEach(piece => piece.addEventListener("dragstart", allowdrag));
 
-        if (this.childElementCount == 1) {return; }
-        console.log('space filled');
+dropZones.forEach(zone => zone.addEventListener('dragover', allowDragOver));
 
-      this.appendChild(document.querySelector(`#${droppedEl}`));
-
-        let track = event.dataTransfer.getData('thetrack');
-
-        document.querySelector(`audio[data-bgref="${track}"]`).play();
-
-    }
-
-
-    function playsound(){
-
-        }
-
-
-
-    function pausesound(){
-        var sounds= document.getElementsByTagName("audio");
-        for (i=0; i<sounds.length; i++ ) sounds[i].pause();
-        console.log('paused')
-
-    }
-
-    function rewindsound(){
-        var sounds = document.getElementsByTagName("audio");
-        for (i=0; i<sounds.length; i++ ) sounds[i].currentTime = 0;
-        console.log('rewinded')
-    }
-
-    function resetbtn(){
-      dropZones.forEach(zone => {
-			if (zone.childElementCount > 0) {
-				dragL.appendChild(zone.firstElementChild);
-			}
-		})
-    AudioEl.forEach(el => el.pause());
-    }
-
-    instrument.forEach(piece => piece.addEventListener("dragstart", allowdrag));
-
-    dropZones.forEach(zone => zone.addEventListener('dragover', allowDragOver));
-
-    dropZones.forEach(zone => zone.addEventListener('drop', allowDrop));
+dropZones.forEach(zone => zone.addEventListener('drop', allowDrop));
